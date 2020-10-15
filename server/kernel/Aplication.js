148b5bb31db99eb0934312b75583bd931b823efe
omar-vasquez-dev <omar.vasquez.dev@gmail.com>
2020-10-14 21:00:01 -0500

    description app

--- server/kernel/Aplication.js
+++ server/kernel/Aplication.js
@@ -5,17 +5,38 @@ const bodyParser = require("koa-bodyparser");
 const cors = require("@koa/cors");
 const Logger = require("koa-logger");
 
-
 class ApplicationProvider {
   register(ioc, namespace) {
     ioc.singleton(namespace, () => {
       const app = new Koa();
+      /**
+       * Corns Policy
+       */
       app.use(cors());
+      /**
+       * Json formating
+       */
       app.use(json());
+
+      /**
+       * Logger Request
+       */
       app.use(new Logger());
+
+      /**
+       * Format post body request.
+       */
       app.use(bodyParser());
+
+      /**
+       * Router domains
+       */
       const router = new KoaRouter();
       app.use(router.routes()).use(router.allowedMethods());
+
+      /**
+       * Application
+       */
       return { app, router };
     });
   }
